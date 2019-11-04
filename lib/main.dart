4a1b63a8d3f20a29844e74cceb1e96c3664cf1ca
Felipe Ambrozini <felipean@ccuec.unicamp.br>
2019-11-04 13:03:35 -0300

    Aplicativo com SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:lost_and_found/views/root_page.dart';
 import 'package:lost_and_found/views/sign_in_page.dart';
 import 'package:lost_and_found/views/sign_up.page.dart';
 import 'package:lost_and_found/views/use_term_page.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -32,5 +33,49 @@ class MyApp extends StatelessWidget {
       },
       home: RootPage(),
     );
+  } 
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return _introScreen();
   }
+}
+
+Widget _introScreen() {
+  return Stack(
+    children: <Widget>[
+      SplashScreen(
+        seconds: 2,
+        gradientBackground: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Color(0xffED213A),
+            Color(0xff93291E)
+          ],
+        ),
+        navigateAfterSeconds: RootPage(),
+        loaderColor: Colors.transparent,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/icon/icon.png"),
+            fit: BoxFit.none,
+          ),
+        ),
+      ),
+    ],
+  );
 }
